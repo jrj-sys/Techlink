75e7e79f3c45b34a3ff72e154f92b32d322ccc7e
jrj-sys <jamesjamel99@gmail.com>
2022-05-09 16:53:27 -0500

    Comment and Reaction models

--- models/index.js
+++ models/index.js
@@ -1,10 +1,11 @@
 const User = require('./User');
 const Post = require('./Post');
-
+const Comment = require('./Comment');
+const Reaction = require('./Reaction');
 
 // associations 
 
 
 
 
-module.exports = { User, Post };
+module.exports = { User, Post, Comment, Reaction };
