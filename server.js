60d5deab98bf9843c0088ba8bb5df0c557d54aae
jrj-sys <jamesjamel99@gmail.com>
2022-05-15 16:44:59 -0500

    reinit session

--- server.js
+++ server.js
@@ -5,19 +5,19 @@ const path = require('path');
 const exphbs = require('express-handlebars');
 const helpers = require('./utils/helpers')
 const hbs = exphbs.create({ helpers });
-// const session = require('express-session')
+const session = require('express-session')
 
-// const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-// const sess = {
-//   secret: process.env.SECRET,
-//   cookie: {},
-//   resave: false,
-//   saveUninitialized: true,
-//   store: new SequelizeStore({
-//     db: sequelize
-//   })
-// };
+const sess = {
+  secret: process.env.SECRET,
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -27,7 +27,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'client')));
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars')
-// app.use(session(sess));
+app.use(session(sess));
 
 app.use(routes)
 
